79d604dfbfbc02c9abeecb857430de831fb223e1
Bokhtiar Tushar <bokhtiartoshar1@gmail.com>
2021-07-05 11:47:07 +0600

    create product page update

--- src/components/admin/product/create.vue
+++ src/components/admin/product/create.vue
@@ -14,13 +14,30 @@
 
     <section class="my-3">
       <form @submit.prevent="formSubmit" action="POST" enctype="multipart/form-data">
-        <div class="form-group">
+
+        <div class=" row">
+          <div class="col-sm-12 col-md-4 col-lg-4">
+            <div class="form-group">
           <label for="">Product Title</label>
           <input v-model="title" type="text" class="form-control" name="title" placeholder="Enter Product Title" id="">
         </div>
+          </div>
+          <div class="col-sm-12 col-md-4 col-lg-4">
+            <div class="form-group">
+          <label for="">Product Code</label>
+          <input v-model="product_code" type="number" class="form-control" name="product_code" placeholder="Enter Product Code" id="">
+        </div>
+          </div>
+          <div class="col-sm-12 col-md-4 col-lg-4">
+            <div class="form-group">
+          <label for="">Product Quantity</label>
+          <input v-model="product_quantity" type="number" class="form-control" name="product_quantity" placeholder="Enter Product Quantity" id="">
+        </div>
+          </div>
+        </div>
 
-        <div class="my-3 row">
-          <div class="col-sm-12 col-md-6 col-lg-6">
+        <div class=" row">
+          <div class=" form-group col-sm-12 col-md-6 col-lg-6">
             <label for="">Select Category</label>
             <select v-model="category_id" class="form-control" name="category_id" id="">
               <option value="">Select Category</option>
@@ -28,7 +45,7 @@
             </select>
           </div>
 
-          <div class="col-sm-12 col-md-6 col-lg-6">
+          <div class=" form-group col-sm-12 col-md-6 col-lg-6">
             <label for="">Select Brand</label>
             <select v-model="brand_id" class="form-control" name="brand_id" id="">
               <option value="1">Category</option>
@@ -38,20 +55,111 @@
         </div>
 
 
-        <div class="my-3">
-          <div class="row">
-            <div class="col-sm-12 col-md-6 col-lg-6">
-              <label for="">Product Price</label>
-              <input v-model="price" type="text" class="form-control" placeholder="Product Price" name="price" id="">
-            </div>
-            <div class="col-sm-12 col-md-6 col-lg-6">
-              <label for="">Product Image</label>
-              <input type="file" class="form-control" v-on:change="onImageChange" name="image" id="">
-            </div>
+          <div class=" row">
+          <div class=" col-sm-12 col-md-4 col-lg-4">
+            <div class="form-group">
+          <label for="">Product Unit</label>
+          <select v-model="product_unit" name="product_unit" class="form-control" id="">
+            <option value="Piece">Piece</option>
+            <option value="Piece">Dozen Box</option>
+            <option value="Piece">Cartoon Box</option>
+            <option value="Meter">Meter</option>
+            <option value="Kilogram">Kilogram</option>
+          </select>
+        </div>
+          </div>
+          <div class="col-sm-12 col-md-4 col-lg-4">
+            <div class="form-group">
+          <label for="">Product Sele Unit</label>
+          <select v-model="product_sell_unit" name="product_sell_unit" class="form-control" id="">
+            <option value="Piece">Piece</option>
+            <option value="Piece">Dozen Box</option>
+            <option value="Piece">Cartoon Box</option>
+            <option value="Meter">Meter</option>
+            <option value="Kilogram">Kilogram</option>
+          </select>
+        </div>
+          </div>
+          <div class=" col-sm-12 col-md-4 col-lg-4">
+            <div class="form-group">
+          <label for="">Product Purchase Unit</label>
+          <select v-model="product_purchase_unit" name="product_sell_unit" class="form-control" id="">
+            <option value="Piece">Piece</option>
+            <option value="Piece">Dozen Box</option>
+            <option value="Piece">Cartoon Box</option>
+            <option value="Meter">Meter</option>
+            <option value="Kilogram">Kilogram</option>
+          </select>
+        </div>
           </div>
         </div>
 
-        <div class="text-center">
+
+
+        <div class=" row">
+          <div class="col-sm-12 col-md-4 col-lg-4">
+            <div class="form-group">
+          <label for="">Product Cost</label>
+          <input v-model="product_cost_price" type="number" class="form-control" name="product_cost_price" placeholder="Enter Product Cost Price" id="">
+        </div>
+          </div>
+          <div class="col-sm-12 col-md-4 col-lg-4">
+            <div class="form-group">
+          <label for="">Product Price</label>
+          <input v-model="price" type="text" class="form-control" name="price" placeholder="Enter Product Title" id="">
+        </div>
+
+          </div>
+          <div class="col-sm-12 col-md-4 col-lg-4">
+            <div class="form-group">
+          <label for="">Product Display in Pos</label> <br>
+          <input type="checkbox" name="" v-model="pos_display" value="1" id="">
+        </div>
+          </div>
+        </div>
+
+
+        <div class="">
+            <div class="form-group">
+              <label for="">Product Image</label>
+              <input type="file" class="form-control" v-on:change="onImageChange" name="image" id="">
+            </div>
+        </div>
+
+        <div class="">
+            <div class="form-group">
+              <label for="">Product Description</label>
+              <textarea v-model="description" name="" class="form-control" id="" cols="30" rows="10"></textarea>
+            </div>
+        </div>
+
+        <div>
+          <input v-model="product_promotion" type="checkbox" value="1" name="" id="">Add Promotional Price
+          <div v-if="product_promotion == 1">
+            <div class="row my-3">
+          <div class="col-sm-12 col-md-4 col-lg-4">
+            <div class="form-group">
+          <label for="">Promotional Price</label>
+          <input v-model="promotional_price" type="number" class="form-control" name="promotional_price" placeholder="Enter Promotional Price" id="">
+        </div>
+          </div>
+          <div class="col-sm-12 col-md-4 col-lg-4">
+            <div class="form-group">
+          <label for="">Promotional Starts Date</label>
+          <input v-model="promotion_start_date" type="date" class="form-control" name="" id="">
+        </div>
+          </div>
+          <div class="col-sm-12 col-md-4 col-lg-4">
+            <div class="form-group">
+          <label for="">Promotional End Date</label> <br>
+          <input v-model="promotion_end_date" type="date" class="form-control" name="" id="">
+        </div>
+          </div>
+        </div>
+          </div>
+        </div>
+
+        <div class="text-center my-2">
           <input class="form-control btn btn-primary" type="submit" value="Create New Product" name="" id="">
         </div>
       </form>
@@ -66,10 +174,22 @@ export default {
    data() {
             return {
               title: '',
+              product_code: '',
+              product_quantity:'',
               category_id : '',
               brand_id : '',
+              product_unit:'',
+              product_sell_unit:'',
+              product_purchase_unit:'',
+              product_cost_price:'',
               price: '',
+              pos_display:'',
               image: '',
+              description:'',
+              product_promotion: '',
+              promotional_price:'',
+              promotion_start_date:'',
+              promotion_end_date:'',
             };
         },
 computed: {
